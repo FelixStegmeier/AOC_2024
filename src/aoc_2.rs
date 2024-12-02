13f62a90c62fa8edf3a35ade46422a52c074a100
Laesterschwester <126001314+Laesterschwester@users.noreply.github.com>
2024-12-02 10:33:25 +0100

    AOC day 2

---
+++ src/aoc_2.rs
@@ -0,0 +1,60 @@
+fn parser(path: &std::path::Path) -> Vec<Vec<i32>>{
+    let s = std::fs::read_to_string(path).unwrap();
+    let mut matrix: Vec<Vec<i32>> = vec!(); 
+    for row in s.split('\n'){
+        let mut buf: Vec<i32> = vec!();
+        for (index, element) in  row.split(' ').enumerate(){
+            buf.push(match element.trim().parse::<i32>(){
+                Ok(ok) => {print!("{} ", ok); ok}
+                Err(_e) => {print!("what"); continue},
+            });
+        }
+        print!("\n");
+        matrix.push(buf);
+    }
+    matrix
+}
+
+fn check_all_increasing_all_decreasing_by_max_3(row: Vec<i32>) -> bool{
+    let mut bad_report = false;
+    let mut increasing = false;
+    let mut decreasing = false;
+    let mut previous: i32 = row[0];
+    for index in 1..row.len(){
+        let diff =  previous - row[index];
+        if diff.abs() <= 0 || diff.abs() > 3{
+            return false;
+        }
+        if diff < 0{
+            decreasing = true;
+        }
+        if diff > 0{
+            increasing = true;
+        }
+        previous = row[index];
+    }
+    (increasing || decreasing) && !(increasing && decreasing)
+}
+
+fn brute_force(mut row: Vec<i32>) -> bool{
+    for i in 0..row.len(){
+        let mut row_clone = row.clone();
+        row_clone.remove(i);
+        if (check_all_increasing_all_decreasing_by_max_3(row_clone)){
+            return true;
+        };
+    }
+    return false
+}
+
+pub fn entrypoint_1(path: &std::path::Path) -> i32{
+    let matrix = parser(path);
+    let mut count = 0;
+    for row in matrix{
+        if brute_force(row) {
+            count += 1;
+        }
+    }
+    print!("\n{}", count);
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod aoc_1;
+mod aoc_2;
 fn main() {
-    aoc_1::entrypoint(std::path::Path::new("C:\\Users\\stegm\\ProgrammingAndOtherProjects\\rust_projects\\AOC_2024\\input_01"));
+    //aoc_1::entrypoint(std::path::Path::new("C:\\Users\\stegm\\ProgrammingAndOtherProjects\\rust_projects\\AOC_2024\\input_01"));
+    aoc_2::entrypoint_1(std::path::Path::new("C:\\Users\\stegm\\Programming\\AOC_2024\\input\\02\\input_02"));
 }
