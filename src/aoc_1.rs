5df40b4d44eddc32c22807472537805c9b132195
Laesterschwester <126001314+Laesterschwester@users.noreply.github.com>
2024-12-04 11:24:34 +0100

    AOC_4_1

--- src/aoc_1.rs
+++ src/aoc_1.rs
@@ -1,38 +1,37 @@
 use std::{io::Read, vec};
 
-fn stuff(mut list_a: Vec<i32>, mut list_b: Vec<i32>) -> i32{
+fn stuff(mut list_a: Vec<i32>, mut list_b: Vec<i32>) -> i32 {
     list_a.sort();
     list_b.sort();
     let mut sum: i32 = 0;
-    for i in 0..list_a.len(){
+    for i in 0..list_a.len() {
         sum += (list_a[i] - list_b[i]).abs();
     }
     sum
 }
 
-fn input_to_list(mut file: std::fs::File){
+fn input_to_list(mut file: std::fs::File) {
     let buffer: &mut [u8] = &mut [];
     file.read(buffer);
 }
 
-fn read_from_fs(path: &std::path::Path) -> [Vec<i32>; 2]{
+fn read_from_fs(path: &std::path::Path) -> [Vec<i32>; 2] {
     let s = std::fs::read_to_string(path).unwrap();
-    let mut list_a: Vec<i32> = vec!();
-    let mut list_b: Vec<i32> = vec!();
+    let mut list_a: Vec<i32> = vec![];
+    let mut list_b: Vec<i32> = vec![];
 
-    for line in s.lines(){
+    for line in s.lines() {
         for (index, element) in line.split("   ").enumerate() {
-            if(index == 0){
+            if (index == 0) {
                 print!("{}   ", element);
                 list_a.push(element.parse::<i32>().unwrap());
-            }
-            else{
+            } else {
                 print!("{}\n", element);
                 list_b.push(element.parse::<i32>().unwrap());
             }
         }
     }
-    return [list_a, list_b]
+    return [list_a, list_b];
 }
 
 pub fn entrypoint(path: &std::path::Path) {
